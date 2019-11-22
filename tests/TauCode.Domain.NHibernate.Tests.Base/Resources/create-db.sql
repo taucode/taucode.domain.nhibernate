b1adfa79cddb2028d09b240cc611d00b72a865ed
Andrey Kovalenko <ak@taucode.com>
2019-11-22 19:27:53 +0200

    Trivia/sql.

--- tests/TauCode.Domain.NHibernate.Tests.Base/Resources/create-db.sql
+++ tests/TauCode.Domain.NHibernate.Tests.Base/Resources/create-db.sql
@@ -9,5 +9,5 @@ CREATE TABLE [event](
 	[id] uniqueidentifier NOT NULL PRIMARY KEY,
 	[user_id] uniqueidentifier NOT NULL,
 	[description] varchar(100) NOT NULL,
-	CONSTRAINT FK_event_user FOREIGN KEY([user_id]) REFERENCES [user]([id])
+	CONSTRAINT [FK_event_user] FOREIGN KEY([user_id]) REFERENCES [user]([id])
 )
